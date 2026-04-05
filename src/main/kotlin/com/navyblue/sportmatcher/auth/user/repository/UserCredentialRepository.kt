f897b1da551ed25589f15de72b448f75a356b0ad
Dawid Zubrowski <aurochs108@gmail.com>
2026-04-05 23:26:03 +0200

    refactor: remove unused imports from UserCredentialRepository

--- src/main/kotlin/com/navyblue/sportmatcher/auth/user/repository/UserCredentialRepository.kt
+++ src/main/kotlin/com/navyblue/sportmatcher/auth/user/repository/UserCredentialRepository.kt
@@ -1,7 +1,5 @@
 package com.navyblue.sportmatcher.auth.user.repository
 
-import com.navyblue.sportmatcher.auth.user.entity.AuthProvider
-import com.navyblue.sportmatcher.auth.user.entity.User
 import com.navyblue.sportmatcher.auth.user.entity.UserCredential
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
