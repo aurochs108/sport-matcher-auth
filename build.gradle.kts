1394a1bd96f28b43618b73ae70c0c09652b8c1ba
🍏NavyBlue - Dawid Żubrowski <39237819+aurochs108@users.noreply.github.com>
2026-04-29 00:26:57 +0200

    fix: added liquibase and import env file in application.yml (#7)
    What has been changed:
    - Added Liquibase missing import.
    - Added import in application.yml for .env file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-liquibase")
     implementation("org.liquibase:liquibase-core")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
