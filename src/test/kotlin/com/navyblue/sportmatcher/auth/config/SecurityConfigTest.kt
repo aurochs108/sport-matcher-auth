88c0abde3308b5b0079488263cb660278a94b25d
Dawid Zubrowski <aurochs108@gmail.com>
2026-05-17 16:48:58 +0200

    code cleaning

--- src/test/kotlin/com/navyblue/sportmatcher/auth/config/SecurityConfigTest.kt
+++ src/test/kotlin/com/navyblue/sportmatcher/auth/config/SecurityConfigTest.kt
@@ -16,12 +16,15 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 
-@WebMvcTest(controllers = [EmailRegistrationController::class, EmailLoginController::class, LogoutController::class])
+@WebMvcTest(controllers = [
+    EmailRegistrationController::class,
+    EmailLoginController::class,
+    LogoutController::class
+])
 @Import(SecurityConfig::class)
-class SecurityConfigTest {
-    @Autowired
-    lateinit var mockMvc: MockMvc
-
+class SecurityConfigTest (
+    @Autowired private val mockMvc: MockMvc,
+) {
     @MockitoBean
     lateinit var emailRegistrationService: EmailRegistrationService
 
